5b69f479a368178f10e110924b23520a19de3d0f
HamadAlmesri <63778523+HamadAlmesri@users.noreply.github.com>
2022-09-05 19:12:50 +0300

    done in dart pad

---
+++ cw-1/cw-1.dart
@@ -0,0 +1,12 @@
+void main() {
+  String firstName = "hamad";
+  String lastName = "almesri";
+  int age = 27; 
+  String hobby = "Programming";
+  
+  
+  
+  
+ print("First Name: $firstName");
+}
+
